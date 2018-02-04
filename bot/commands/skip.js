e266cb3836e7be8d62e04672ea1d61c9b8efa9bc
TheBeepMaster <jordikeijzers@gmail.com>
2018-02-04 13:21:00 +0100

    Add files via upload

---
+++ bot/commands/skip.js
@@ -0,0 +1,21 @@
+const play = require("./play.js");
+
+exports.run = function(client, message, args) {
+    if (message.member.voiceChannel) {
+        const connection = client.voiceConnections.find("id", message.member.voiceChannel.id);
+        if (connection) {
+            play.skip();
+        } else {
+            return message.reply("There's no voice connection found.");
+        };
+    } else {
+        return message.reply("You need to be in a voice channel to stop playing music.");
+    };
+};
+
+exports.help = {
+    "name": "Stop",
+    "usage": "",
+    "permission-level": 1,
+    "catergory": "Music",
+}
