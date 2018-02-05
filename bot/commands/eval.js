70a63941a54ea6a99cf986f4e8f0013d22bf73d9
TheBeepMaster <jordikeijzers@gmail.com>
2018-02-05 15:45:49 +0100

    Create eval.js

---
+++ bot/commands/eval.js
@@ -0,0 +1,31 @@
+function clean(text) {
+    if (typeof(text) === "string") {
+        return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    } else {
+        return text;
+    };
+};
+
+exports.run = function(client, message, args) {
+    if (message.author.id !== "208666671194439681")
+        return message.reply("You are not authorized to execute this command. This command is restricted to the bot creator.");
+
+    try {
+        let evaled = eval(args.join(" "));
+
+        if (typeof evaled !== "string") {
+            console.log(evaled);
+        };
+
+        message.channel.send(clean(evaled), {code: "xl"});
+    } catch(err) {
+        message.channel.send("ERROR: ```xl" + err + "\n```", {code: "xl"});
+    }
+};
+
+exports.help = {
+    "name": "Eval",
+    "usage": "<Code>",
+    "permission-level": 2,
+    "catergory": "Owner Only",
+};
