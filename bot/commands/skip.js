5b4e3c7426f3119c1d7a0ec45998b57b82603557
TheBeepMaster <jordikeijzers@gmail.com>
2018-02-11 12:19:39 +0100

    Update skip.js

--- bot/commands/skip.js
+++ bot/commands/skip.js
@@ -2,12 +2,7 @@ const play = require("./play.js");
 
 exports.run = function(client, message, args) {
     if (message.member.voiceChannel) {
-        const connection = client.voiceConnections.find("id", message.member.voiceChannel.id);
-        if (connection) {
-            play.skip();
-        } else {
-            return message.reply("There's no voice connection found.");
-        };
+        play.skip();
     } else {
         return message.reply("You need to be in a voice channel to stop playing music.");
     };
@@ -18,4 +13,4 @@ exports.help = {
     "usage": "",
     "permission-level": 1,
     "catergory": "Music",
-}
+};
